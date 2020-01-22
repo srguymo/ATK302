8cd200f4865e40d5bd374c1867c3ed121124e67b
srguymo <54365346+srguymo@users.noreply.github.com>
2020-01-22 14:24:11 -0600

    Update sketch.js

--- p5/Week2/Trees/sketch.js
+++ p5/Week2/Trees/sketch.js
@@ -10,12 +10,40 @@ function draw() {
   noStroke();
   fill('#FFEE93');
   ellipse(100, 100, 90, 90);
+
+  fill('#fcfffd');
+  ellipse(300, 100, 50, 50);
+  //clouds
+  fill('#fcfffd');
+  ellipse(320, 100, 50, 50);
+  fill('#fcfffd');
+  ellipse(320, 80, 50, 50);
+  fill('#fcfffd');
+  ellipse(330, 100, 100, 50);
+  fill('#fcfffd');
+  ellipse(580, 150, 50, 50);
+  fill('#fcfffd');
+  ellipse(595, 150, 70, 50);
+  fill('#fcfffd');
+  ellipse(590, 140, 50, 50);
+
+
+
+
+  //trees and shit
   fill('#ADF7B6');
   rect(0, 350, 750, 63);
+  fill(102);
+  rect(450, 330, 43, 43);
+  fill('#2ac374');
+  triangle(500, 18, 398, 360, 560, 360);
+  fill(102);
+  rect(330, 330, 43, 43);
   fill('#77EA85');
   triangle(360, 18, 240, 360, 430, 360);
-  fill(102);
-  rect(81, 81, 63, 63);
+
+//  fill(102);
+//  rect(330, 340, 43, 43);
 
   // fill(204);
   // triangle(18, 18, 18, 360, 81, 360);

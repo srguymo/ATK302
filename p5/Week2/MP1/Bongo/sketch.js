b8604d60a30d4cec42ff79f9c0b5fac85770ba95
srguymo <54365346+srguymo@users.noreply.github.com>
2020-01-23 11:25:47 -0600

    kekin

--- p5/Week2/MP1/Bongo/sketch.js
+++ p5/Week2/MP1/Bongo/sketch.js
@@ -1,8 +1,6 @@
 function setup() {
   // put setup code here
-createCanvas(1000,800);
-
-
+createCanvas(1000, 800);
 
 
 }
@@ -17,8 +15,8 @@ background (100);
    }
 
 
+}
 
-
-
-
+function mouseReleased() {
+  console.log(mouseX + ", " +mouseY);
 }

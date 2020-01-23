33488735a4bcc947a513e60a69761544fb9ea9c1
srguymo <54365346+srguymo@users.noreply.github.com>
2020-01-23 11:16:40 -0600

    h

---
+++ p5/Week2/MP1/Bongo/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  // put setup code here
+createCanvas(1000,800);
+
+
+
+
+}
+
+function draw() {
+  // put drawing code here
+background (100);
+  if (mouseIsPressed) {
+     ellipse(50, 50, 50, 50);
+   } else {
+     rect(25, 25, 50, 50);
+   }
+
+
+
+
+
+
+}

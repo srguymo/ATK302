be8ec962e44815b167eca3f80f6978c2bddcebb3
srguymo <54365346+srguymo@users.noreply.github.com>
2020-01-23 11:35:31 -0600

    Update sketch.js

--- p5/Week2/MP1/Bongo/sketch.js
+++ p5/Week2/MP1/Bongo/sketch.js
@@ -8,12 +8,15 @@ createCanvas(1000, 800);
 function draw() {
   // put drawing code here
 background (100);
+
   if (mouseIsPressed) {
      ellipse(50, 50, 50, 50);
    } else {
      rect(25, 25, 50, 50);
    }
-
+   fill ('#');
+   textSize (30) ;
+     text(mouseX + "," + mouseY,300,50) ;
 
 }
 

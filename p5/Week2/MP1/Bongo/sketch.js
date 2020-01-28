1b3e72b4d572ff4686b8fbb50004fc758cb9da33
srguymo <54365346+srguymo@users.noreply.github.com>
2020-01-28 11:18:35 -0600

    Update sketch.js
    o70

--- p5/Week2/MP1/Bongo/sketch.js
+++ p5/Week2/MP1/Bongo/sketch.js
@@ -1,13 +1,13 @@
 function setup() {
   // put setup code here
-createCanvas(1000, 800);
+createCanvas(800, 500);
 
 
 }
 
 function draw() {
   // put drawing code here
-background (100);
+background ('#A6EBC9');
 //as
   if (mouseIsPressed) {
      ellipse(50, 50, 50, 50);
@@ -19,10 +19,34 @@ background (100);
    textSize (30) ;
      text(mouseX + "," + mouseY,300,50) ;
 //building bongo
-fill('#fcfffd');
+//noStroke();
+fill('#F9D4F0');
 ellipse(304, 163, 70, 70);
-fill('#');
+fill('#F9D4F0');
 rect(300, 134, 70, 53);
+fill('#F0E2A3');
+ellipse(303, 148, 20, 20);
+fill('#F9D4F0');
+triangle(288, 133, 236, 29, 285, 77);
+fill('#F9D4F0');
+triangle(301, 128, 300, 29, 319, 79);
+fill('#F9B1E7');
+ellipse(299, 290, 60, 60);
+fill('#F9B1E7');
+rect(298, 320, 85, 20);
+fill('#F9D4F0');
+rect(280, 193, 50, 100);
+fill('#664E4C');
+rect(357, 136, 20, 20);
+fill('#F9D4F0');
+ellipse(280, 290, 60, 60);
+fill('#F9D4F0');
+rect(275, 320, 85, 20);
+fill('#F9D4F0');
+triangle(278, 260, 98, 168, 162, 265);
+fill('#F9D4F0');
+rect(295, 212, 85, 20);
+
 
 
 }

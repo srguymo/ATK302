77595f986bd646d8d26ef7cab9e5e7c32a587a25
srguymo <54365346+srguymo@users.noreply.github.com>
2020-02-13 11:44:42 -0600

    dsf

--- p5/Week5/timer/sketch.js
+++ p5/Week5/timer/sketch.js
@@ -1,7 +1,24 @@
+
+var timer = 200;
+
+
 function setup() {
   // put setup code here
+  createCanvas (800,800);
+  background ('green');
 }
 
 function draw() {
   // put drawing code here
+//  textSize (100) ;
+//  text(timer, width/2, height/2);
+timer = timer -1;
+if (timer == 0) {
+  background (random(256),random(256),random(256));
+  timer = 200;
+}
+
+
+
+
 }

93f6ef31e168131b8b25067b03d5a53e73f92cbd
srguymo <54365346+srguymo@users.noreply.github.com>
2020-03-03 11:53:47 -0600

    gfj

--- p5/Week6/MP2/sketch.js
+++ p5/Week6/MP2/sketch.js
@@ -1,9 +1,23 @@
+//the bouncing image code is from  icm's code for bouncing ball from p5js
+
 var myState = 0 ;
 var timer = 0;
+var mic;
+var vol;
+var zoom;
+//var y= 0;
+let x = 320;
+let y = 180;
+let xspeed = 10;
+let yspeed = 4;
+let r = 25;
 
 function setup() {
   // put setup code here
   createCanvas(800, 800) ;
+  zoom = loadImage ("assets/zoom.png")
+  //mic = new p5.AudioIn();
+  //mic.start();
 }
 
 function draw() {
@@ -12,7 +26,7 @@ function draw() {
   switch(myState) {
     case 0 :
     background('red');
-    text("TITLE SCREEN", 100, 100) ;
+    text("Chad the Cat", 100, 100) ;
 timer++ ;
 if(timer >200){
   myState =1;
@@ -25,19 +39,41 @@ if(timer >200){
 
     case 1:
     background('green');
-    text("I'm in state 1", 100, 100) ;
+    text("Chad the cat is a soft black cat.",100,100);
+    text("His favorite thing in the whole world is sleeping on the window seat in his house. ", 200, 200) ;
+    text ("Chad lives a peaceful life and has never caused problems in his life.",300,300);
     break ;
 
     case 2:
     background('red');
-    text("I'm in state 2", 100, 100) ;
+    text("Chad hears a loud noise and gets spooked! What is with that look on his face? What is he going to do?", 100, 100) ;
     break ;
 
     case 3:
+    background ('purple');
+    text ("Chad is running around the living room like a fucking maniac.",100,100);
+//fill('pink');
+  // ellipse(x, y, r*2, r*2);
+  image (zoom,x,y,r*2, r*2);
+ x += xspeed;
+ y += yspeed;
+ if (x > width - r || x < r) {
+   xspeed = -xspeed;
+ }
+ if (y > height - r || y < r) {
+   yspeed = -yspeed;
+ }
     break ;
 
     case 4:
+    background ('pink');
+    text("Chad has singlehandedly destroyed the living room.", 100,100);
     break ;
+
+    case 5:
+    background ('yellow');
+    text ("Chad only causes problems on purpouse",100,100);
+    break;
 
   }
 
@@ -45,7 +81,7 @@ if(timer >200){
 
 function mouseReleased() {
   myState = myState + 1;
-  if (myState > 4) {
+  if (myState > 5) {
     myState = 0 ;
   }
 }

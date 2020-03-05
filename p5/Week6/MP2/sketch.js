e849526fec390e52f58c429420ab20c31b4042ea
srguymo <54365346+srguymo@users.noreply.github.com>
2020-03-04 21:57:31 -0600

    safjashljkfhjjkhsf
    sada

--- p5/Week6/MP2/sketch.js
+++ p5/Week6/MP2/sketch.js
@@ -2,30 +2,45 @@
 
 var myState = 0 ;
 var timer = 0;
-var mic;
-var vol;
+// image declarations
 var zoom;
-//var y= 0;
-let x = 320;
-let y = 180;
+var test;
+var bkg;
+var chad1;
+var chad2;
+var dist;
+var chad5;
+var chad3;
+var tchad;
+//asorted
+let x = 520;
+let y = 480;
 let xspeed = 10;
 let yspeed = 4;
 let r = 25;
 
 function setup() {
-  // put setup code here
+
   createCanvas(800, 800) ;
-  zoom = loadImage ("assets/zoom.png")
+  zoom = loadImage ("assets/zootin.png")
+  test = loadImage ("assets/test1.jpg")
+  bkg= loadImage("assets/LivingroomA.jpg")
+  chad1 =loadImage ("assets/chad1.png")
+  chad2= loadImage("assets/chad2.png")
+  dist= loadImage ("assets/destroy.jpg")
+  chad5= loadImage("assets/chad5.png")
+  chad3= loadImage("assets/problemchad.png")
+  tchad= loadImage("assets/titlechad.jpg")
   //mic = new p5.AudioIn();
   //mic.start();
 }
 
 function draw() {
-  // put drawing code here
 
   switch(myState) {
     case 0 :
     background('red');
+    image (tchad,0,0,800,800);
     text("Chad the Cat", 100, 100) ;
 timer++ ;
 if(timer >200){
@@ -38,22 +53,38 @@ if(timer >200){
     break ;
 
     case 1:
-    background('green');
+      image(bkg, 0, 0, 800, 800);
+      image(chad1,0,0,800,800);
+    //background('green');
     text("Chad the cat is a soft black cat.",100,100);
-    text("His favorite thing in the whole world is sleeping on the window seat in his house. ", 200, 200) ;
-    text ("Chad lives a peaceful life and has never caused problems in his life.",300,300);
+    text("His favorite thing in the whole world is sleeping on the window seat in his house. ", 100, 130) ;
+
+    //image(test, 0, 0, 800, 800);
+    timer++ ;
+    if(timer >200){
+      myState =2;
+      timer = 0;
+    }
     break ;
 
     case 2:
     background('red');
+    image(bkg,0,0,800,800);
+    image (chad2,0,0,800,800);
+
     text("Chad hears a loud noise and gets spooked! What is with that look on his face? What is he going to do?", 100, 100) ;
+    timer++ ;
+    if(timer >200){
+      myState =3;
+      timer = 0;
+    }
     break ;
 
     case 3:
     background ('purple');
+    image (bkg,0,0,800,800);
     text ("Chad is running around the living room like a fucking maniac.",100,100);
-//fill('pink');
-  // ellipse(x, y, r*2, r*2);
+//moving cat
   image (zoom,x,y,r*2, r*2);
  x += xspeed;
  y += yspeed;
@@ -63,16 +94,34 @@ if(timer >200){
  if (y > height - r || y < r) {
    yspeed = -yspeed;
  }
+ timer++ ;
+ if(timer >400){
+   myState =4;
+   timer = 0;
+ }
     break ;
 
     case 4:
     background ('pink');
+      image(dist, 0, 0, 800, 800);
+        image(chad5, 0, 0, 800, 800);
     text("Chad has singlehandedly destroyed the living room.", 100,100);
+    timer++ ;
+    if(timer >200){
+      myState =5;
+      timer = 0;
+    }
     break ;
 
     case 5:
     background ('yellow');
+      image(chad3, 0, 0, 800, 800);
     text ("Chad only causes problems on purpouse",100,100);
+    timer++ ;
+    if(timer >200){
+      myState =0;
+      timer = 0;
+    }
     break;
 
   }
